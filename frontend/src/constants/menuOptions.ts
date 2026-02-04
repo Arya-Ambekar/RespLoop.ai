70a14516b50bcefa49010fc8813cf9245ed24bbe
Arya Ambekar <aryaambekar17@gmail.com>
2026-02-04 22:37:28 +0530

    added menu part in admin panel page

---
+++ frontend/src/constants/menuOptions.ts
@@ -0,0 +1,19 @@
+import { MessagesSquare, Tag, ChartNoAxesCombined } from "lucide-react";
+
+export const MENU_OPTIONS = [
+  {
+    id: "conversations",
+    label: "Conversations",
+    Icon: MessagesSquare,
+  },
+  {
+    id: "tickets",
+    label: "Tickets",
+    Icon: Tag,
+  },
+  {
+    id: "improve-ai",
+    label: "Improve AI",
+    Icon: ChartNoAxesCombined,
+  },
+];
--- frontend/src/pages/AdminPanelPage/AdminPanelPage.tsx
+++ frontend/src/pages/AdminPanelPage/AdminPanelPage.tsx
@@ -1,11 +1,54 @@
 import AdminPanelHeader from "../../components/AdminPanelHeader/AdminPanelHeader";
 import "./AdminPanelPage.css";
+import { MENU_OPTIONS } from "../../constants/menuOptions";
+import { useState } from "react";
 
 const AdminPanelPage = () => {
+  const [selectedMenuOption, setSelectedMenuOption] = useState("conversations");
+
   return (
     <div className="admin-panel-page-wrapper">
-      <div>
+      <div className="admin-panel-page-header">
         <AdminPanelHeader />
+      </div>
+      <div className="admin-panel-body-wrapper">
+        <div className="menu-wrapper">
+          {MENU_OPTIONS.map(({ id, label, Icon }) => {
+            const selected = selectedMenuOption === id;
+
+            return (
+              <div
+                key={id}
+                className={selected ? "selected-menu-option" : "menu-option"}
+                onClick={() => setSelectedMenuOption(id)}
+              >
+                <div
+                  className={
+                    selected
+                      ? "selected-menu-option-icon-wrapper"
+                      : "menu-option-icon-wrapper"
+                  }
+                >
+                  <Icon
+                    className={
+                      selected
+                        ? "selected-menu-option-icon"
+                        : "menu-option-icon"
+                    }
+                  />
+                </div>
+                <p
+                  className={
+                    selected ? "selected-menu-option-name" : "menu-option-name"
+                  }
+                >
+                  {label}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="admin-panel-main-body-wrapper"></div>
       </div>
     </div>
   );
