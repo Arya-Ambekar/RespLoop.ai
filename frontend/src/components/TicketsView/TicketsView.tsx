11dd0d9f84526edcc2bed48b41b045093463de70
Arya Ambekar <aryaambekar17@gmail.com>
2026-02-05 18:35:31 +0530

    tickets view UI completed

--- frontend/src/components/TicketsView/TicketsView.tsx
+++ frontend/src/components/TicketsView/TicketsView.tsx
@@ -1,5 +1,62 @@
+import { useState } from "react";
+import "./TicketsView.css";
+import { TICKET_STATUS_FILTER_OPTIONS } from "../../constants/ticketStatusOptions";
+import FilterDropdown from "../FilterDropdown/FilterDropdown.tsx";
+import { TICKETS_DUMMY_DATA } from "../../constants/ticketsViewDummyData.ts";
+
 const TicketsView = () => {
-  return <div>TicketsView</div>;
+  const [selectedTicketStatus, setSelectedTicketStatus] = useState(
+    TICKET_STATUS_FILTER_OPTIONS[0],
+  );
+
+  return (
+    <div className="tickets-view-wrapper">
+      <FilterDropdown
+        dropdownList={TICKET_STATUS_FILTER_OPTIONS}
+        selectedValue={selectedTicketStatus}
+        onSelect={(value: string) => {
+          console.log("filter clicked");
+          setSelectedTicketStatus(value);
+        }}
+      />
+      <div className="tickets-content-table-wrapper">
+        <table className="tickets-view-table">
+          <thead className="table-header">
+            <tr>
+              <th>Conversation Id</th>
+              <th>Reason</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody className="table-body">
+            {TICKETS_DUMMY_DATA.map((ticket) => [
+              <tr
+                key={ticket.id}
+                onClick={() => console.log("table row clicked", ticket.id)}
+              >
+                <td>{ticket.serial_id}</td>
+                <td>{ticket.reason}</td>
+                <td>
+                  <div
+                    className={`tickets-status 
+                      ${ticket.status === "Open" ? "open" : null} 
+                      ${ticket.status === "Closed" ? "closed" : null}`}
+                  >
+                    {ticket.status}
+                  </div>
+                </td>
+              </tr>,
+            ])}
+          </tbody>
+        </table>
+      </div>
+      <div className="pagination">
+        <button onClick={() => console.log("prev button clicked")}>Prev</button>
+        <p>Page 1 of 5</p>
+        <button onClick={() => console.log("next button clicked")}>Next</button>
+      </div>
+    </div>
+  );
 };
 
 export default TicketsView;
---
+++ frontend/src/constants/ticketsViewDummyData.ts
@@ -0,0 +1,62 @@
+export const TICKETS_DUMMY_DATA = [
+  {
+    id: 1,
+    serial_id: "CONVO-001",
+    reason: "Application is crashing or not responding",
+    status: "Open",
+  },
+  {
+    id: 2,
+    serial_id: "CONVO-002",
+    reason: "Payment was deducted but order not confirmed",
+    status: "Closed",
+  },
+  {
+    id: 3,
+    serial_id: "CONVO-003",
+    reason: "Customer is facing issues while completing checkout",
+    status: "Closed",
+  },
+  {
+    id: 4,
+    serial_id: "CONVO-004",
+    reason: "Incorrect or missing information displayed on the dashboard",
+    status: "Open",
+  },
+  {
+    id: 5,
+    serial_id: "CONVO-005",
+    reason: "User is unable to log in to the application",
+    status: "Open",
+  },
+  {
+    id: 6,
+    serial_id: "CONVO-006",
+    reason: "Notifications are not being received",
+    status: "Closed",
+  },
+  {
+    id: 7,
+    serial_id: "CONVO-007",
+    reason: "Order history is not loading properly",
+    status: "Open",
+  },
+  {
+    id: 8,
+    serial_id: "CONVO-008",
+    reason: "Incorrect pricing shown on checkout page",
+    status: "Closed",
+  },
+  {
+    id: 9,
+    serial_id: "CONVO-009",
+    reason: "Profile details are not updating",
+    status: "Open",
+  },
+  {
+    id: 10,
+    serial_id: "CONVO-010",
+    reason: "Search functionality is returning incorrect results",
+    status: "Closed",
+  },
+];
