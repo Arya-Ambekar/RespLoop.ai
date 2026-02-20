f8fd4bcd7e1716b1286633aad2075d1a3d5a9e2f
Arya Ambekar <aryaambekar17@gmail.com>
2026-02-20 22:03:26 +0530

    some changes

--- backend/utilities/reusableFunctions/DateFormatter.ts
+++ backend/utilities/reusableFunctions/DateFormatter.ts
@@ -1,4 +1,4 @@
-export function formatDateTime(date: Date) {
+export const formatDateTime = async (date: Date) => {
   const months = [
     "Jan",
     "Feb",
@@ -21,4 +21,4 @@ export function formatDateTime(date: Date) {
   const minutes = String(date.getMinutes()).padStart(2, "0");
 
   return `${day} ${month} ${year}, ${hours}:${minutes}`;
-}
+};
